db3b8e610c320ca3b197520c076fce5d13d7151e
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-12-06 18:38:51 -0300

    add

--- test2.py
+++ test2.py
@@ -20,16 +20,21 @@ def get_local_ip_address(target='10.255.255.255'):
     return IP
 
 def receive_messages(sock):
-    global message_list
+    global message_list, members
     while True:
         try:
-            data, addr = sock.recvfrom(1024)  # Buffer size 1024 bytes
-            message = f"{addr[0]} falou: {data.decode()}"
+            data, addr = sock.recvfrom(1024)
+            if data.decode().startswith("/novo_membro"):
+                _, new_ip, new_port = data.decode().split()
+                members.add((new_ip, int(new_port)))
+                message = f"Novo membro adicionado: {new_ip}:{new_port}"
+            else:
+                message = f"{addr[0]} falou: {data.decode()}"
             message_list.append(message)
             print_message_list()
         except:
             print("Erro ao receber a mensagem.")
-            break
+            return
 
 def print_message_list():
     global message_list
@@ -39,22 +44,30 @@ def print_message_list():
     print("(----------------------------------------------------)")
     print("Escreva sua mensagem: ", end="")
 
-def add_member(ip, port):
+def add_member(ip, port, sock):
     global members
-    members.add((ip, port))
+    new_member = (ip, port)
+    members.add(new_member)
+    # Informa os outros membros sobre o novo membro
+    for member in members:
+        if member != new_member:
+            try:
+                sock.sendto(f"/novo_membro {ip} {port}".encode(), member)
+            except:
+                print(f"Erro ao informar {member[0]}:{member[1]} sobre o novo membro")
+
     print(f"Membro {ip}:{port} adicionado com sucesso.")
 
 def main():
     global message_list, members
     local_ip = get_local_ip_address()
-    print("Seu ip é: "+ local_ip)
+    print("Seu IP é: " + local_ip)
     port = int(input("Digite a porta para usar no chat: "))
+    members.add((local_ip, port))
 
-    # Configuração do socket UDP
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((local_ip, port))
 
-    # Iniciando thread para receber mensagens
     threading.Thread(target=receive_messages, args=(sock,), daemon=True).start()
 
     print_message_list()
@@ -63,18 +76,18 @@ def main():
 
         if message.startswith("/add_membro"):
             _, member_ip, member_port = message.split()
-            add_member(member_ip, int(member_port))
-            print_message_list()
+            add_member(member_ip, int(member_port), sock)
             continue
 
         message_list.append(f"Você: {message}")
         print_message_list()
 
         for member in members:
-            try:
-                sock.sendto(message.encode(), member)
-            except:
-                print(f"Erro ao enviar mensagem para {member[0]}:{member[1]}")
+            if member != (local_ip, port):
+                try:
+                    sock.sendto(message.encode(), member)
+                except:
+                    print(f"Erro ao enviar mensagem para {member[0]}:{member[1]}")
 
 if __name__ == "__main__":
     main()
