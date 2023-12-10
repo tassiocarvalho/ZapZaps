585c77c706689a592d32c44c262687454d106a10
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2023-12-09 22:43:19 -0300

    add

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,16 @@
-# Use uma imagem base oficial do Python
 FROM python:3.9-slim
 
-# Defina o diretório de trabalho dentro do container
 WORKDIR /app
 
-# Copie o script Python para o diretório de trabalho no container
 COPY chat.py .
 COPY vetorclock.py .
+#COPY criptografia.py .
+#COPY requirements.txt .
 
-# Informe ao Docker que a aplicação escuta na porta 12345
-EXPOSE 12345
+#RUN pip install --no-cache-dir -r requirements.txt
 
-# Comando para executar o script Python quando o container iniciar
+#EXPOSE 12345
+
 CMD ["python", "./chat.py"]
-
 #docker build -t chat .
 #docker run -it chat
---
+++ criptografia.py
@@ -0,0 +1,49 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+
+class Criptografia:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.public_key = self.private_key.public_key()
+
+    def get_public_key(self):
+        # Retorna a chave pública em formato serializado
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    @staticmethod
+    def encrypt_message(message, public_key):
+        # Encripta a mensagem usando a chave pública
+        public_key = serialization.load_pem_public_key(
+            public_key,
+            backend=default_backend()
+        )
+        encrypted = public_key.encrypt(
+            message.encode(),
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return encrypted
+
+    def decrypt_message(self, encrypted_message):
+        # Decripta a mensagem usando a chave privada
+        decrypted = self.private_key.decrypt(
+            encrypted_message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return decrypted.decode()
